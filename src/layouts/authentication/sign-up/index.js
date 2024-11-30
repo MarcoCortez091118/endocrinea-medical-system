146e95b6640babe22ed09e949d23b8a8b5aa29fb
WendySanchezV <wendy.sanchez.tr@vmasideas.com>
2024-11-30 12:36:16 -0600

    Fix password input icon and border styles

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -7,9 +7,6 @@ import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-
-// Material-UI icons
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
@@ -33,17 +30,16 @@ function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState({});
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const [focusedInput, setFocusedInput] = useState(""); // Track which input is focused
 
   const handleSetAgreement = () => setAgreement(!agreement);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     const validationErrors = validateForm(name, email, password, agreement);
-    
+
     if (Object.keys(validationErrors).length === 0) {
       console.log("Formulario enviado exitosamente");
     } else {
@@ -51,18 +47,15 @@ function SignUp() {
     }
   };
 
-  const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
-
-  const getInputStyle = (inputName) => ({
-    border: `1px solid ${focusedInput === inputName ? "#E0040B" : "#ccc"}`,
-    paddingRight: "40px", // Space for the icon to fit inside the input's contoured box
-  });
-
   return (
     <BasicLayout title="Bienvenido" image={curved6}>
       <Card>
         <SoftBox p={3} mb={1} textAlign="center">
-          <SoftTypography variant="h5" fontWeight="medium" sx={{ color: "#E0040B !important" }}>
+          <SoftTypography
+            variant="h5"
+            fontWeight="medium"
+            sx={{ color: "#E0040B !important" }}
+          >
             Regístrate
           </SoftTypography>
         </SoftBox>
@@ -75,12 +68,13 @@ function SignUp() {
                 placeholder="Nombre"
                 error={!!errors.name}
                 fullWidth
-                onFocus={() => setFocusedInput("name")}
-                onBlur={() => setFocusedInput("")}
-                style={getInputStyle("name")}
               />
               {errors.name && (
-                <SoftTypography variant="caption" color="error" sx={{ paddingTop: "5px", display: "block" }}>
+                <SoftTypography
+                  variant="caption"
+                  color="error"
+                  sx={{ paddingTop: "5px", display: "block" }}
+                >
                   {errors.name}
                 </SoftTypography>
               )}
@@ -93,48 +87,51 @@ function SignUp() {
                 placeholder="Correo electrónico"
                 error={!!errors.email}
                 fullWidth
-                onFocus={() => setFocusedInput("email")}
-                onBlur={() => setFocusedInput("")}
-                style={getInputStyle("email")}
               />
               {errors.email && (
-                <SoftTypography variant="caption" color="error" sx={{ paddingTop: "5px", display: "block" }}>
+                <SoftTypography
+                  variant="caption"
+                  color="error"
+                  sx={{ paddingTop: "5px", display: "block" }}
+                >
                   {errors.email}
                 </SoftTypography>
               )}
             </SoftBox>
-            <SoftBox mb={2} sx={{ position: "relative" }}>
+            <SoftBox mb={2} position="relative">
               <SoftInput
-                type={passwordVisible ? "text" : "password"}
+                type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Contraseña"
                 error={!!errors.password}
                 fullWidth
-                onFocus={() => setFocusedInput("password")}
-                onBlur={() => setFocusedInput("")}
-                style={getInputStyle("password")}
+                sx={{
+                  paddingRight: "40px", 
+                }}
               />
-              {/* Eye icon outside the input but inside the border */}
               <IconButton
-                onClick={togglePasswordVisibility}
+                onClick={() => setShowPassword(!showPassword)}
                 edge="end"
                 sx={{
                   position: "absolute",
-                  right: "10px",
                   top: "50%",
+                  right: "10px",
                   transform: "translateY(-50%)",
-                  color: focusedInput === "password" ? "#E0040B" : "#ccc",
                 }}
               >
-                {passwordVisible ? <VisibilityOff /> : <Visibility />}
+                {showPassword ? <Visibility /> : <VisibilityOff />}
               </IconButton>
-              {errors.password && (
-                <SoftTypography variant="caption" color="error" sx={{ paddingTop: "5px", display: "block" }}>
-                  {errors.password}
-                </SoftTypography>
-              )}
             </SoftBox>
+            {errors.password && (
+              <SoftTypography
+                variant="caption"
+                color="error"
+                sx={{ paddingTop: "5px", display: "block" }}
+              >
+                {errors.password}
+              </SoftTypography>
+            )}
             <SoftBox display="flex" alignItems="center">
               <Checkbox checked={agreement} onChange={handleSetAgreement} />
               <SoftTypography
@@ -156,17 +153,34 @@ function SignUp() {
               </SoftTypography>
             </SoftBox>
             {errors.agreement && (
-              <SoftTypography variant="caption" color="error" sx={{ paddingTop: "5px", display: "block", textAlign: "center" }}>
+              <SoftTypography
+                variant="caption"
+                color="error"
+                sx={{
+                  paddingTop: "5px",
+                  display: "block",
+                  textAlign: "center",
+                }}
+              >
                 {errors.agreement}
               </SoftTypography>
             )}
             <SoftBox mt={4} mb={1}>
-              <SoftButton variant="gradient" color="dark" fullWidth type="submit">
+              <SoftButton
+                variant="gradient"
+                color="dark"
+                fullWidth
+                type="submit"
+              >
                 Regístrate
               </SoftButton>
             </SoftBox>
             <SoftBox mt={3} textAlign="center">
-              <SoftTypography variant="button" color="text" fontWeight="regular">
+              <SoftTypography
+                variant="button"
+                color="text"
+                fontWeight="regular"
+              >
                 ¿Ya tienes una cuenta?&nbsp;
                 <SoftTypography
                   component={Link}
@@ -176,7 +190,7 @@ function SignUp() {
                   fontWeight="bold"
                   textGradient
                 >
-                  Inicia sesión
+                  Inicia sesi&oacute;n
                 </SoftTypography>
               </SoftTypography>
             </SoftBox>
