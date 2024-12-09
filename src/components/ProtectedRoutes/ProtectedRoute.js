35b599aae627a56fdb6d1bca8dcf165e2b0cf25d
BryanPichon <bryan.pichon.tr@vmasideas.com>
2024-12-08 23:21:15 -0600

    verified code

--- src/components/ProtectedRoutes/ProtectedRoute.js
+++ src/components/ProtectedRoutes/ProtectedRoute.js
@@ -3,9 +3,8 @@ import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "context/AuthContext";
 
-function ProtectedRoute({ children, }) {
+function ProtectedRoute({ children }) {
     const { isAuthenticated } = useAuth();
-
     return isAuthenticated ? children : <Navigate to="/authentication/sign-in" />;
 }
 
