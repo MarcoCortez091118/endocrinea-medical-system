6ce90804f3fc7a2d334f990549a9bc77bf82da24
WendySanchezV <wendy.sanchez.tr@vmasideas.com>
2024-12-09 10:21:24 -0600

    feat(table-pagination): implement pagination to display 10 rows per page

---
+++ src/layouts/tables/CustomPagination.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+export default function CustomPagination({ page, totalPages, onPageChange }) {
+  return (
+    <Stack spacing={2} alignItems="center" sx={{ mt: 2 }}>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={(event, value) => onPageChange(value)}
+        siblingCount={1}
+        boundaryCount={1}
+        color="standard"
+        sx={{
+          "& .MuiPaginationItem-root": {
+            color: "grey",
+          },
+          "& .MuiPaginationItem-root.Mui-selected": {
+            color: "#FFFFFF",
+            backgroundColor: "#E0040B",
+          },
+          "& .MuiPaginationItem-root:hover": {
+            backgroundColor: "rgba(224, 4, 11, 0.7)",
+          },
+          "& .MuiPaginationItem-ellipsis": {
+            color: "grey",
+          },
+        }}
+      />
+    </Stack>
+  );
+}
+
+CustomPagination.propTypes = {
+  page: PropTypes.number.isRequired,       
+  totalPages: PropTypes.number.isRequired, 
+  onPageChange: PropTypes.func.isRequired, 
+};
