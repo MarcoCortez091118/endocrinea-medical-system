7e272713e2786a651bb1218312bb5fd326ca4570
BryanPichon <bryan.pichon.tr@vmasideas.com>
2024-12-16 16:58:11 -0600

    change icons in routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
 import ClinicalForm from "layouts/FormsHistoryPsychological";
+import HistorialEvolucion from "layouts/historial-evolucion";
 import Shop from "examples/Icons/Shop";
 import Office from "examples/Icons/Office";
 import Settings from "examples/Icons/Settings";
@@ -14,7 +15,7 @@ import SpaceShip from "examples/Icons/SpaceShip";
 import CustomerSupport from "examples/Icons/CustomerSupport";
 import CreditCard from "examples/Icons/CreditCard";
 import Cube from "examples/Icons/Cube";
-
+import HistoryIcon from '@mui/icons-material/History';
 
 
 import ProtectedRoute from "components/ProtectedRoutes/ProtectedRoute";
@@ -48,10 +49,23 @@ const routes = (isAuthenticated) => [
   },
   {
     type: "collapse",
+    name: "historial evolucion",
+    key: "historialevolucion",
+    route: "/historial-evolucion",
+    icon: <HistoryIcon size="12px" />,
+    component: (
+      <ProtectedRoute>
+        <HistorialEvolucion />
+      </ProtectedRoute>
+    ),
+    noCollapse: true,
+  },
+  {
+    type: "collapse",
     name: "Historial psicológia",
     key: "ClinicalForm",
     route: "/FormsHistoryPsychological",
-    icon: <Office size="12px" />,
+    icon: <HistoryIcon size="12px" />,
     component: (
       <ProtectedRoute>
         <ClinicalForm />
@@ -62,40 +76,40 @@ const routes = (isAuthenticated) => [
   { type: "title", title: "Account Pages", key: "account-pages" },
   ...(isAuthenticated
     ? [
-        {
-          type: "collapse",
-          name: "Perfil",
-          key: "profile",
-          route: "/profile",
-          icon: <CustomerSupport size="12px" />,
-          component: (
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          ),
-          noCollapse: true,
-        },
-      ]
+      {
+        type: "collapse",
+        name: "Perfil",
+        key: "profile",
+        route: "/profile",
+        icon: <CustomerSupport size="12px" />,
+        component: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+        noCollapse: true,
+      },
+    ]
     : [
-        {
-          type: "collapse",
-          name: "Iniciar sesión",
-          key: "sign-in",
-          route: "/authentication/sign-in",
-          icon: <Document size="12px" />,
-          component: <SignIn />,
-          noCollapse: true,
-        },
-        {
-          type: "collapse",
-          name: "Registrarme",
-          key: "sign-up",
-          route: "/authentication/sign-up",
-          icon: <SpaceShip size="12px" />,
-          component: <SignUp />,
-          noCollapse: true,
-        },
-      ]),
+      {
+        type: "collapse",
+        name: "Iniciar sesión",
+        key: "sign-in",
+        route: "/authentication/sign-in",
+        icon: <Document size="12px" />,
+        component: <SignIn />,
+        noCollapse: true,
+      },
+      {
+        type: "collapse",
+        name: "Registrarme",
+        key: "sign-up",
+        route: "/authentication/sign-up",
+        icon: <SpaceShip size="12px" />,
+        component: <SignUp />,
+        noCollapse: true,
+      },
+    ]),
 ];
 
 export default routes;
