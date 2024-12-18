bc8a961eb054898017d35c1d3f3c9120f75a01aa
BryanPichon <bryan.pichon.tr@vmasideas.com>
2024-12-18 13:07:53 -0600

    create historial clinico

---
+++ src/layouts/historial-clinico/index.js
@@ -0,0 +1,44 @@
+// Importaciones necesarias
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+// Soft UI Dashboard React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+
+function HistorialClinico() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <SoftBox py={3}>
+        <SoftBox mb={3}>
+          <Card>
+            <SoftBox display="flex" flexDirection="column" alignItems="flex-start" p={3}>
+              <SoftTypography variant="h4">Historia Clínica -</SoftTypography>
+              <SoftTypography variant="h4">Endocrinea Care</SoftTypography>
+              <SoftTypography variant="subtitle2" fontWeight="medium" mt={3}>
+                Estimado paciente los siguientes datos de contacto y antecedentes médicos recabados
+                en el presente documento serán utilizados para llenar su historial médico.
+              </SoftTypography>
+              <SoftTypography variant="subtitle2" fontWeight="medium" mt={2}>
+                Todos sus datos serán tratados con total confidencialidad, la información sera
+                utilizada única y exclusivamente para mejorar la calidad de la atención durante su
+                consulta y brindarle un mejor servicio.
+              </SoftTypography>
+              <SoftTypography variant="subtitle2" fontWeight="medium" mt={3}>
+                Dr. Francisco Javier Porquillo C.P. 10550033 / 12467290 <br />
+                Dra.
+              </SoftTypography>
+            </SoftBox>
+          </Card>
+        </SoftBox>
+      </SoftBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default HistorialClinico;
