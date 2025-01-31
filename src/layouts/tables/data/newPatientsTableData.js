fc64cf553e56ec6d546f0dd8f3352bfaeb69d96d
WendySanchezV <wendy.sanchez.tr@vmasideas.com>
2025-01-30 20:44:00 -0600

    a bug was fixed

--- src/layouts/tables/data/newPatientsTableData.js
+++ src/layouts/tables/data/newPatientsTableData.js
@@ -26,13 +26,26 @@ export default function useNewPatientsTableData() {
           foto: <SoftAvatar src={team2} size="sm" variant="rounded" />,
           id: patient.id ?? "No especificado",
           nombre: `${patient.first_name ?? "No first name"} ${patient.last_name ?? "No last name"}`,
-          teléfono: patient.phone ?? "Teléfono no proporcionado",
-          correo: patient.email ?? "Correo no proporcionado",
+          teléfono: patient.phoneNumber ?? "Teléfono no proporcionado", // CORREGIDO: `phoneNumber`
+          correo: patient.email ?? "Correo no proporcionado", // CORREGIDO: `email`
           género: patient.gender ?? "Género no especificado",
           estatus: patient.status ?? "Estatus no proporcionado",
           Acciones: (
             <Button
-              onClick={() => navigate("/PatientDetails", { state: { patient } })}
+              onClick={() =>
+                navigate("/PatientDetails", {
+                  state: {
+                    patient: {
+                      name: `${patient.first_name ?? "No first name"} ${patient.last_name ?? "No last name"}`,
+                      id: patient.id ?? "No especificado",
+                      email: patient.email ?? "Correo no proporcionado", // CORREGIDO
+                      phone: patient.phoneNumber ?? "Teléfono no proporcionado", // CORREGIDO
+                      gender: patient.gender ?? "Género no especificado",
+                      status: patient.status ?? "Estatus no proporcionado",
+                    },
+                  },
+                })
+              }
               variant="text"
               color="primary"
             >
@@ -40,6 +53,7 @@ export default function useNewPatientsTableData() {
             </Button>
           ),
         }));
+        
 
         setData({
           newColumns: [
