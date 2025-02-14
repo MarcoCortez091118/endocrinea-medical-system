a6c98a272ade4f955a3d53a1cd913954c6b73329
BryanPPichon <bryan_acuario25@hotmail.com>
2025-02-14 03:44:45 -0600

    fix path redirected

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,34 +1,60 @@
-/*
 import React, { createContext, useState, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
-  const [userData, setUserData] = useState({ name: "", email: "", token: "" });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({ firstName: "", lastName: "", email: "", token: "" });
+
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedData = localStorage.getItem("authData");
+    const lastPath = localStorage.getItem("lastPath") || "/tables";
+
     if (storedData) {
-      const parsedData = JSON.parse(storedData);
-      setIsAuthenticated(true);
-      setUserData(parsedData);
+      try {
+        const parsedData = JSON.parse(storedData);
+        if (parsedData?.token && parsedData?.role) {
+          setIsAuthenticated(true);
+          setUserData(parsedData);
+          navigate(lastPath);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error("Error parsing authData:", error);
+        setIsAuthenticated(false);
+      }
     } else {
       setIsAuthenticated(false);
     }
+    setLoading(false);
   }, []);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      localStorage.setItem("lastPath", location.pathname); // Guardar la última página
+    }
+  }, [location, isAuthenticated]);
 
   const login = (data) => {
     setIsAuthenticated(true);
     setUserData(data);
     localStorage.setItem("authData", JSON.stringify(data));
+    navigate(localStorage.getItem("lastPath") || "/tables");
   };
 
   const logout = () => {
     setIsAuthenticated(false);
-    setUserData({ name: "", email: "", token: "" });
+    setUserData({ firstName: "", lastName: "", email: "", token: "" });
     localStorage.removeItem("authData");
+    localStorage.removeItem("lastPath");
+    navigate("/authentication/sign-in");
   };
 
   return (
@@ -43,4 +69,3 @@ AuthProvider.propTypes = {
 };
 
 export const useAuth = () => useContext(AuthContext);
-*/
