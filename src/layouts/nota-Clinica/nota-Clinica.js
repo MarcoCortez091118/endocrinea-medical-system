a1e795445eb4428c32cbf5b54ce80a2e3b982d5a
Zeus Guzman <chegus2101@gmail.com>
2025-02-17 02:52:54 -0600

    The design of the clinical note record has been changed

--- src/layouts/nota-Clinica/nota-Clinica.js
+++ src/layouts/nota-Clinica/nota-Clinica.js
@@ -40,11 +40,7 @@ function NotaClinica() {
     return localDate.toLocaleString("es-ES", {
       year: "numeric",
       month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false, // Formato 24 horas
+      day: "numeric"
     });
   };
 
